fd41a6616837fa54c9f39c24120dc717f106346a
Andrew <AndrewDmit2021@gmail.com>
2024-12-18 01:15:53 -0600

    intial calendar

--- app/(root)/calendar/page.tsx
+++ app/(root)/calendar/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+"use client"
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
 
-const Calendar = () => {
+export default function Calendar() {
   return (
-    <div>Calendar</div>
+    <div className="w-100 h-screen">
+          <FullCalendar
+            height={"auto"}
+            plugins={[dayGridPlugin, timeGridPlugin]} // Initialize calendar with required plugins.
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
+            }} // Set header toolbar options.
+            initialView="dayGridMonth" // Initial view mode of the calendar.
+            editable={true} // Allow events to be edited.
+            selectable={true} // Allow dates to be selectable.
+            selectMirror={true} // Mirror selections visually.
+            dayMaxEvents={true} // Limit the number of events displayed per day.
+            initialEvents={
+              typeof window !== "undefined"
+                ? JSON.parse(localStorage.getItem("events") || "[]")
+                : []
+            }
+            events={[{title : "event 1" , start : "2024-12-19T04:00:00", end : "2024-12-19T09:00:00",  allDay : false,},
+            {title : "event 2" , start : "2024-12-19T04:00:00", end : "2024-12-19T09:00:00",  allDay : false, color : "red"}
+        ]}
+          />
+    </div>
+
   )
 }
-
-export default Calendar
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import "../globals.css";
 import { ClerkProvider, UserButton } from "@clerk/nextjs";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "../components/shared/sidebar/Sidebar";
-import { getUser, isVerrified } from "@/lib/actions/user.actions";
+import { getUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Verification from "../components/shared/Verification";
@@ -17,44 +17,41 @@ export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) 
-  {
-    const user = await currentUser();
-    if(!user){
-      redirect('/sign-in');
-    }
-    
-    const monUser : any = await getUser({id : user.id});
-    if(!monUser || !monUser.onboarded){
-      redirect('/onboarding')
-      return null
-    }
-    
-    const verrified = monUser.verified;
+}>) {
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
 
-    return (
-        <html lang="en">
-          <head>
-            <title>Portal</title>
-          </head>
-          <ClerkProvider>
-          { verrified ?
-          (
+  const monUser: any = await getUser({ id: user.id });
+  if (!monUser || !monUser.onboarded) {
+    redirect("/onboarding");
+    return null;
+  }
+
+  const verified = monUser.verified;
+
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <title>Portal</title>
+        </head>
+        <body className="flex h-screen w-screen">
+          {verified ? (
+            <SidebarProvider>
+              {/* Sidebar */}
               
-              <body
-                className={`antialiased`}
-              >
-              <SidebarProvider>
-                <AppSidebar/>
-                {/* <Topbar/> */}
-                <SidebarTrigger/>
-                {children}
-             </SidebarProvider>
-              </body>
-            ) :           
-            (<Verification/>)
-          }
-          </ClerkProvider>
-        </html>
+              <AppSidebar/>
+              {/* Main Content */}
+              <SidebarTrigger/>
+              <div className="flex-1 overflow-auto p-4">{children}</div>
+            </SidebarProvider>
+          ) : (
+            <Verification />
+          )}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
