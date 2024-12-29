8e853667b30ab84b639a2c217fa90860efab9192
Andrew Dmitrievsky <144502433+AndrewDmit04@users.noreply.github.com>
2024-12-28 21:45:14 -0600

    roll back to clerk

--- app/(root)/sheets/page.tsx
+++ app/(root)/sheets/page.tsx
@@ -1,14 +1,13 @@
-import AdminPunch from '@/app/components/shared/punch/AdminPunch';
-import UserPunch from '@/app/components/shared/punch/UserPunch';
 import AdminHoursDashboard from '@/app/components/shared/TimeSheets/AdminSheet';
 import UserHoursDashboard from '@/app/components/shared/TimeSheets/UserSheet';
-import { getRole, getUser } from '@/lib/actions/user.actions'
-import { currentUser } from '@clerk/nextjs/server'
+import { getRole } from '@/lib/actions/user.actions'
+import { currentUser } from '@clerk/nextjs/server';
 import React from 'react'
 
 const Sheet = async() => {
+
   const user = await currentUser();
-  if(!user){return}
+    if(!user){return}
   const role = await getRole({id : user.id});
   const isAdmin = role === "Director"
   return (
